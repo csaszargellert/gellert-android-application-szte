59c77b71250e335b2b1a00673e3dd2cf7fd5e833
Gellért Császár <csaszargellert@gmail.com>
2024-05-04 15:34:26 +0200

    refactor: activites layouts

---
+++ app/src/main/java/com/app/nailappointment/view/activites/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.app.nailappointment.view.activites;
+
+import android.os.Bundle;
+
+import com.app.nailappointment.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.app.nailappointment.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
---
+++ app/src/main/java/com/app/nailappointment/view/activites/MyAppointmentsActivity.java
@@ -0,0 +1,20 @@
+package com.app.nailappointment.view.activites;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.app.nailappointment.R;
+
+public class MyAppointmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_appointments);
+    }
+}
